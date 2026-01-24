5ac616252efd6f2b8c993454c9b56ec0559d9a55
lfrallon <loretofrallon@gmail.com>
2026-01-24 21:02:28 +0800

    chore: 🛂 updated auth config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,12 +4,26 @@ import { db } from '../db/index.ts'
 import * as schema from '../db/schema/index.ts'
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
+  emailAndPassword: {
+    enabled: true,
+  },
+  trustedOrigins: ['http://localhost:3000'],
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 7 * 24 * 60 * 60, // 7 days cache duration
+      strategy: 'jwe', // can be "jwt" or "compact"
+      refreshCache: true, // Enable stateless refresh
+    },
+  },
+  account: {
+    storeStateStrategy: 'cookie',
+    storeAccountCookie: true, // Store account data after OAuth flow in a cookie (useful for database-less flows)
+  },
   database: drizzleAdapter(db, {
     provider: 'pg', // or "pg" or "mysql"
     schema,
   }),
-  emailAndPassword: {
-    enabled: true,
-  },
-  secret: process.env.BETTER_AUTH_SECRET,
 })
