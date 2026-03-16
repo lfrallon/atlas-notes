474c43fb367763e488609598ea0a84aa3d957afa
Reto2 <loretofrallon@gmail.com>
2026-03-16 20:13:40 +0800

    fix cesium setup for maps route in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import { fileURLToPath, URL } from 'url'
-
-import tailwindcss from '@tailwindcss/vite'
 import { cloudflare } from '@cloudflare/vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
+import viteReact from '@vitejs/plugin-react'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+
 
 const config = defineConfig({
   resolve: {
@@ -22,7 +23,23 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: [
+            'node_modules/cesium/Build/Cesium/Workers',
+            'node_modules/cesium/Build/Cesium/Assets',
+            'node_modules/cesium/Build/Cesium/ThirdParty',
+            'node_modules/cesium/Build/Cesium/Widgets',
+          ],
+          dest: 'cesium',
+        },
+      ],
+    }),
   ],
+  define: {
+    CESIUM_BASE_URL: JSON.stringify('/cesium'),
+  },
 })
 
 export default config
