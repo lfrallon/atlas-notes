0ad3d13ae055bb459355f1eff6888416fbb7eba7
Reto2 <loretofrallon@gmail.com>
2026-04-04 13:53:05 +0800

    Update Cesium static copy targets to explicit globs (#22)

--- vite.config.ts
+++ vite.config.ts
@@ -34,13 +34,20 @@ const config = defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: [
-            'node_modules/cesium/Build/Cesium/Workers',
-            'node_modules/cesium/Build/Cesium/Assets',
-            'node_modules/cesium/Build/Cesium/ThirdParty',
-            'node_modules/cesium/Build/Cesium/Widgets',
-          ],
-          dest: 'cesium',
+          src: 'node_modules/cesium/Build/Cesium/Workers/**/*',
+          dest: 'cesium/Workers',
+        },
+        {
+          src: 'node_modules/cesium/Build/Cesium/Assets/**/*',
+          dest: 'cesium/Assets',
+        },
+        {
+          src: 'node_modules/cesium/Build/Cesium/ThirdParty/**/*',
+          dest: 'cesium/ThirdParty',
+        },
+        {
+          src: 'node_modules/cesium/Build/Cesium/Widgets/**/*',
+          dest: 'cesium/Widgets',
         },
       ],
     }),
