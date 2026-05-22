248079b05a825fbc35bda3b8729e3a6c48dedcc8
lfrallon <loretofrallon@gmail.com>
2026-05-22 21:27:40 +0800

    feat(hook): ✨ added user access custom hook Get user access operational scope.

---
+++ src/hooks/use-user-access.ts
@@ -0,0 +1,50 @@
+import { queryOptions, useQuery } from '@tanstack/react-query'
+
+// types
+import type { Permission } from '@/utils/auth'
+
+type TFetchUserAccess = {
+  queryKey: [
+    string,
+    {
+      baseUrl: string
+    },
+  ]
+}
+
+type UserAccessPage = {
+  id: string | null
+  role: string
+  permissions: Permission[]
+}
+
+const USER_ACCESS_API_BASE_URL =
+  import.meta.env.VITE_FASTIFY_API_URL ?? 'http://localhost:3006/api/v1'
+
+async function getUserAccess({ queryKey }: TFetchUserAccess) {
+  const [, { baseUrl }] = queryKey
+
+  const response = await fetch(`${baseUrl}/user/access`, {
+    credentials: 'include',
+  })
+
+  const data: UserAccessPage = await response.json()
+  return data
+}
+
+export function useUserAccess() {
+  const userAccessOptions = queryOptions<UserAccessPage, Error>({
+    queryKey: ['userAccess', { baseUrl: USER_ACCESS_API_BASE_URL }],
+    queryFn: async ({ queryKey }) =>
+      await getUserAccess({
+        queryKey: queryKey as [
+          string,
+          {
+            baseUrl: string
+          },
+        ],
+      }),
+  })
+
+  return useQuery(userAccessOptions)
+}
